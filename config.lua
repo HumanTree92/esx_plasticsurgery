968fc744d75419d49d1abcc4262ee7530621d15e
William Plunkett Jr <joey.plunkett92@yahoo.com>
2019-03-28 23:41:17 -0500

    Update

--- config.lua
+++ config.lua
@@ -1,19 +1,30 @@
 Config = {}
 Config.Locale = 'en'
 
-Config.Price = 5000
-
 Config.MarkerType   = 1
 Config.DrawDistance = 100.0
-Config.MarkerSize   = {x = 1.5, y = 1.5, z = 1.0}
+Config.MarkerSize   = {x = 2.0, y = 2.0, z = 1.0}
 Config.MarkerColor  = {r = 102, g = 102, b = 204}
 
-Config.Zones = {}
+Config.BlipPlasticSurgery = {
+	Sprite = 403,
+	Color = 0,
+	Display = 2,
+	Scale = 1.0
+}
+
+Config.Price = 5000 -- Edit to your liking.
+
+Config.EnableUnemployedOnly = true -- If true it will only show Blips to Unemployed Players | false shows it to Everyone.
+Config.EnableBlips = true -- If true then it will show blips | false does the Opposite.
+Config.EnablePeds = true -- If true then it will add Peds on Markers | false does the Opposite.
 
 Config.Locations = {
-	{ x = 402.85, y = -998.4, z = -100.0 }, -- Staff Room
-	{ x = 279.95, y = -1337.27, z = 23.55 } -- Hospital Bottom Floor
+	--{ x = 402.85, y = -998.4, z = -100.0, heading = 180.0 }, -- Police Booking Room
+	{ x = 260.3, y = -1343.68, z = 23.54, heading = 257.66 } -- Hospital Bottom Floor
 }
+
+Config.Zones = {}
 
 for i=1, #Config.Locations, 1 do
 	Config.Zones['Shop_' .. i] = {
